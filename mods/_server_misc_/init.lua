08b4bea82ddbde4da526d6a975f1942fd3c40ddb
Amaz1 <charliejordan34@gmail.com>
2014-06-10 22:12:33 +0100

    Added everything!

---
+++ mods/_server_misc_/init.lua
@@ -0,0 +1,18 @@
+---------------------
+-- Server Misc Mod --
+---------------------
+
+--News
+dofile(minetest.get_modpath("_server_misc_").."/news.lua")
+
+--Rules
+dofile(minetest.get_modpath("_server_misc_").."/rules.lua")
+
+-- Aliases
+dofile(minetest.get_modpath("_server_misc_").."/aliases.lua")
+
+-- Chatlog
+dofile(minetest.get_modpath("_server_misc_").."/chatlog.lua")
+
+-- Username filter
+dofile(minetest.get_modpath("_server_misc_").."/username_filter.lua")
---
+++ mods/currency/init.lua
@@ -0,0 +1,6 @@
+dofile(minetest.get_modpath("currency").."/craftitems.lua")
+dofile(minetest.get_modpath("currency").."/shop.lua")
+dofile(minetest.get_modpath("currency").."/barter.lua")
+dofile(minetest.get_modpath("currency").."/crafting.lua")
+dofile(minetest.get_modpath("currency").."/income.lua")
+
